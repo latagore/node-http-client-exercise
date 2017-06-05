7545122ad4878629ef33e0eead475e16264c59f8
Steven Xu <s@stevenxu.xyz>
2017-06-05 16:06:43 +0000

    do step 2

---
+++ httpClient.js
@@ -0,0 +1,28 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+  
+  https.get(requestOptions, function (response) {
+    let output = "";
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      output += data;
+    });
+    
+    response.on('end', function (data) {
+      console.log(output);
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
