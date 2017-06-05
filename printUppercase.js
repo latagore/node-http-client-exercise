696e26255c92534a46b3529729b98fb858e34fa3
Steven Xu <s@stevenxu.xyz>
2017-06-05 16:16:45 +0000

    add tests

---
+++ printUppercase.js
@@ -0,0 +1,8 @@
+var getHTML = require('./httpClient');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+getHTML(requestOptions, (data) => console.log(data.toUpperCase()));
